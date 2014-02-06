4b5d3a24b6940027a9a0aaf01ca9f8a9d3f3f1b2
EnigMoiD <evan.dorsky@icloud.com>
2014-02-06 12:03:46 -0500

    Homework 2: Added regex search to find_track

--- hw02/find_track.c
+++ hw02/find_track.c
@@ -8,6 +8,7 @@ License: Creative Commons Attribution-ShareAlike 3.0
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <regex.h>
 
 #define NUM_TRACKS 5
 
@@ -33,12 +34,32 @@ void find_track(char search_for[])
     }
 }
 
+// Matches a string against an extended regex
+// Based on function reference and example code from
+// http://pubs.opengroup.org/onlinepubs/7908799/xsh/regexec.html
+int match(char *string, char *pattern) {
+    regex_t re;
+    if (regcomp(&re, pattern, REG_EXTENDED) != 0)
+        return 0;
+    int status = regexec(&re, string, (size_t) 0, NULL, 0);
+    regfree(&re);
+    if (status != 0) {
+        return 0;
+    }
+    return 1;
+}
+
 // Finds all tracks that match the given pattern.
 //
 // Prints track number and title.
 void find_track_regex(char pattern[])
 {
-    // TODO: fill this in
+    int i;
+    for (i=0; i<NUM_TRACKS; i++) {
+    if (match(tracks[i], pattern)) {
+        printf("Track %i: '%s'\n", i, tracks[i]);
+    }
+    }
 }
 
 // Truncates the string at the first newline, if there is one.
@@ -59,8 +80,8 @@ int main (int argc, char *argv[])
     fgets(search_for, 80, stdin);
     rstrip(search_for);
 
-    find_track(search_for);
-    //find_track_regex(search_for);
+    // find_track(search_for);
+    find_track_regex(search_for);
 
     return 0;
 }
