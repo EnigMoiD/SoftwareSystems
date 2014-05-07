e3ace7f8fced8f8ca43f250e58687585c2da89fb
EnigMoiD <evan.dorsky@icloud.com>
2014-05-07 00:30:22 -0400

    Working, reasonable concise fizzbuzz

---
+++ fizzbuzz/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+	for (int i = 1; i < 101; i++) {
+		if (i % 3 == 0) printf("Fizz");
+		if (i % 5 == 0) printf("Buzz");
+		if (i % 5 != 0 && i % 3 != 0) printf("%d", i);
+		printf("\n");
+	}
+	return 0;
+}
